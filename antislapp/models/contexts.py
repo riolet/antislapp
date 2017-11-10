b3521833c8b3adefc9e7da3d089f0fe5c40d30ab
Joe Pelz <joe.pelz@gmail.com>
2017-11-10 15:02:22 -0800

    get and set contexts seems to work

--- antislapp/models/contexts.py
+++ antislapp/models/contexts.py
@@ -8,22 +8,29 @@ class Contexts():
     def __init__(self, session_id):
         self.base_url = "https://api.dialogflow.com/v1"
         self.endpoint = "/contexts"
-        self.session_id = session_id
+        self.session_id = session_id[:36]
         self.token = index.CLIENT_ACCESS_TOKEN
 
     def get(self):
-        # context_model = apiai.Request(self.token, self.base_url, self.endpoint, ["sessionId={}".format(self.session_id)])
-
-        #url = "{}{}?v=20150910&sessionId={}".format(self.base_url, self.endpoint, self.session_id)
-        url = "{}{}?v=20150910&sessionId={}".format(self.base_url, self.endpoint, self.session_id)
+        url = "{}{}?sessionId={}".format(self.base_url, self.endpoint, self.session_id)
         headers = {
             'content-type': 'application/json',
             'authorization': 'bearer {}'.format(self.token)
         }
-        print("trying url: {}".format(url))
         response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            raise KeyError(response.content)
 
-        return response
+        return response.content
 
-    def set(self):
-        pass
+    def set(self, contexts):
+        url = "{}{}?sessionId={}".format(self.base_url, self.endpoint, self.session_id)
+        headers = {
+            'content-type': 'application/json',
+            'authorization': 'bearer {}'.format(self.token)
+        }
+        response = requests.post(url, contexts, headers=headers)
+        if response.status_code != 200:
+            raise KeyError(response.content)
+
+        return response.content
